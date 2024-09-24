ab4e92c8afe13ab30f700984d7ba333acbea3d35
iuryzanardi <95059026+iuryzanardi@users.noreply.github.com>
2024-09-24 18:47:31 -0300

    fix(auth): resolve login flash persistence issue
    - Fixed issue where login error messages persisted after page refresh.
    - Uploaded initial backend implementation for login feature, excluding database integration.
    - Added detailed error handling for invalid login attempts, preparing for further development and testing.
    This version highlights the fix, emphasizes that the login implementation is still in.

--- app.py
+++ app.py
@@ -1,11 +1,28 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template, request, redirect, url_for, flash
 
 app = Flask(__name__)
 
-@app.route('/login')
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+
+@app.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        if username == 'admin' and password == 'admin': 
+            flash('Correto')
+            return redirect(url_for('dashboard'))
+            
+        else:
+            flash('Nome de usuário ou senha inválidos.')
+            return redirect(url_for('login'))
+    
     return render_template('login.html')
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
