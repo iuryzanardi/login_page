1614a0d46de5ef9fe9c09d12a4e597f22dda1d20
iuryzanardi <95059026+iuryzanardi@users.noreply.github.com>
2024-10-02 21:48:11 -0300

    Add login verification with database.
     - Implemented login functionality that verifies user credentials against the database.

--- app.py
+++ app.py
@@ -1,6 +1,21 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import check_password_hash
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:password@localhost/login_app'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(80), unique=True, nullable=False) 
+    password = db.Column(db.String(120), nullable=False) 
+
+    def __repr__(self):
+        return f'<Usuario {self.username}, {self.password}>'
 
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
@@ -9,11 +24,11 @@ def login():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
-
-        if username == 'admin' and password == 'admin': 
-            flash('Correto')
+        
+        user = Users.query.filter_by(username=username).first()
+        
+        if user and check_password_hash(user.password, password):
             return redirect(url_for('dashboard'))
-            
         else:
             flash('Nome de usuário ou senha inválidos.')
             return redirect(url_for('login'))
@@ -26,3 +41,4 @@ def dashboard():
 
 if __name__ == '__main__':
     app.run(debug=True)
+    
